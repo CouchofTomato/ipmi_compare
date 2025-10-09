5939ba11367e545ec1f234f50ddf6d786ea3ed63
Austin Mason <austinpmason@gmail.com>
2025-10-09 15:29:49 +0100

    Create PlanGeographicCoverArea join table model

---
+++ spec/factories/plan_geographic_cover_areas.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :plan_geographic_cover_area do
+    plan { nil }
+    geographic_cover_area { nil }
+  end
+end
---
+++ spec/models/plan_geographic_cover_area_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe PlanGeographicCoverArea, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
