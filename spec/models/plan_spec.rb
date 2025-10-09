d9fcd45b76160ac41be78dc53f4a6c084fa5fb12
Austin Mason <austinpmason@gmail.com>
2025-10-09 16:20:50 +0100

    Add associations between Plan and GeographicCoverArea

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe Plan, type: :model do
   subject(:plan) { create(:plan) }
 
+  it { expect(plan).to have_many(:plan_geographic_cover_areas).dependent(:destroy) }
+  it { expect(plan).to have_many(:geographic_cover_areas).through(:plan_geographic_cover_areas) }
+
   it { expect(plan).to validate_presence_of :name }
   it { expect(plan).to validate_presence_of :min_age }
   it { expect(plan).to validate_numericality_of(:min_age).is_greater_than_or_equal_to(0).only_integer }
