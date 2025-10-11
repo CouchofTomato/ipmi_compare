8bff6c7d401c87fdd013017045168ac88c0511a7
Austin Mason <austinpmason@gmail.com>
2025-10-11 18:26:54 +0100

    Add Country model validations

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,6 @@
 class Country < ApplicationRecord
   belongs_to :region
+
+  validates :name, presence: true
+  validates :code, presence: true
 end
--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Country, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:country) { create(:country) }
+
+  it { expect(country).to belong_to(:region) }
+
+  it { expect(country).to validate_presence_of :name }
+  it { expect(country).to validate_presence_of :code }
 end
