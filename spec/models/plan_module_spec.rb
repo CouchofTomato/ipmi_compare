2456416d4083d61820408f0ab718dce1803cf1c6
Austin Mason <austinpmason@gmail.com>
2025-10-12 16:02:20 +0100

    Add has_many relationship between PlanModule and BenefitLimitGroup

--- spec/models/plan_module_spec.rb
+++ spec/models/plan_module_spec.rb
@@ -7,5 +7,6 @@ RSpec.describe PlanModule, type: :model do
   it { is_expected.to belong_to(:depends_on_module).class_name('PlanModule').optional }
   it { is_expected.to belong_to(:module_group) }
   it { is_expected.to have_many(:dependent_modules).class_name('PlanModule').with_foreign_key('depends_on_module_id').dependent(:nullify) }
+  it { is_expected.to have_many(:benefit_limit_groups).dependent(:destroy) }
   it { is_expected.to validate_presence_of(:name) }
 end
