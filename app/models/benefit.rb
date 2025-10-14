241d56793ef81ebec8df24fbb19ecc56419eaca9
Austin Mason <austinpmason@gmail.com>
2025-10-14 21:15:31 +0100

    Add benefit validations

--- app/models/benefit.rb
+++ app/models/benefit.rb
@@ -1,2 +1,17 @@
 class Benefit < ApplicationRecord
+  enum :category, {
+    inpatient: 0,
+    outpatient: 1,
+    therapies: 2,
+    maternity: 3,
+    dental: 4,
+    optical: 5,
+    medicines: 6,
+    evacuation: 7,
+    repatriation: 8,
+    wellness: 9
+  }
+
+  validates :name, presence: true
+  validates :category, presence: true
 end
