db3ddb894ffde290133d820b87aebad7aa6f424a
Austin Mason <austinpmason@gmail.com>
2025-10-14 20:59:07 +0100

    Create Benefit model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_13_190723) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_14_195810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -25,6 +25,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_13_190723) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["plan_module_id"], name: "index_benefit_limit_groups_on_plan_module_id"
+  end
+
+  create_table "benefits", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "category", null: false
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "countries", force: :cascade do |t|
---
+++ spec/factories/benefits.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :benefit do
+    name { "MyString" }
+    category { 1 }
+    description { "MyText" }
+  end
+end
