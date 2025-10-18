92406a033d3a6d76d46566b61ef254dfb1daf752
Austin Mason <austinpmason@gmail.com>
2025-10-18 21:36:18 +0100

    Add PlanModule assocations to CostShare

--- app/models/plan_module.rb
+++ app/models/plan_module.rb
@@ -2,8 +2,18 @@ class PlanModule < ApplicationRecord
   belongs_to :plan
   belongs_to :module_group
   belongs_to :depends_on_module, class_name: "PlanModule", optional: true
+
   has_many :dependent_modules, class_name: "PlanModule", foreign_key: "depends_on_module_id", dependent: :nullify
+
   has_many :benefit_limit_groups, dependent: :destroy
+
+  has_many :cost_shares, as: :scope, dependent: :destroy
+  has_many :deductibles, -> { where(cost_share_type: :deductible) },
+           class_name: "CostShare", as: :scope
+  has_many :coinsurances, -> { where(cost_share_type: :coinsurance) },
+           class_name: "CostShare", as: :scope
+  has_many :excesses, -> { where(cost_share_type: :excess) },
+           class_name: "CostShare", as: :scope
 
   validates :name, presence: true
 end
--- spec/models/plan_module_spec.rb
+++ spec/models/plan_module_spec.rb
@@ -3,10 +3,17 @@ require 'rails_helper'
 RSpec.describe PlanModule, type: :model do
   subject(:plan_module) { create(:plan_module) }
 
-  it { is_expected.to belong_to(:plan) }
-  it { is_expected.to belong_to(:depends_on_module).class_name('PlanModule').optional }
-  it { is_expected.to belong_to(:module_group) }
-  it { is_expected.to have_many(:dependent_modules).class_name('PlanModule').with_foreign_key('depends_on_module_id').dependent(:nullify) }
-  it { is_expected.to have_many(:benefit_limit_groups).dependent(:destroy) }
-  it { is_expected.to validate_presence_of(:name) }
+  #== Associations ===========================================================
+  it { (expect(plan_module)).to belong_to(:plan) }
+  it { (expect(plan_module)).to belong_to(:depends_on_module).class_name('PlanModule').optional }
+  it { (expect(plan_module)).to belong_to(:module_group) }
+  it { (expect(plan_module)).to have_many(:dependent_modules).class_name('PlanModule').with_foreign_key('depends_on_module_id').dependent(:nullify) }
+  it { (expect(plan_module)).to have_many(:benefit_limit_groups).dependent(:destroy) }
+  it { (expect(plan_module)).to have_many(:cost_shares).dependent(:destroy) }
+  it { (expect(plan_module)).to have_many(:deductibles).class_name("CostShare") }
+  it { (expect(plan_module)).to have_many(:coinsurances).class_name("CostShare") }
+  it { (expect(plan_module)).to have_many(:excesses).class_name("CostShare") }
+
+  #== Validations ===========================================================
+  it { (expect(plan_module)).to validate_presence_of(:name) }
 end
