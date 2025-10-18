c2ea3becdf65af01ad0d3e435f3dd3c78567f5a8
Austin Mason <austinpmason@gmail.com>
2025-10-18 20:50:03 +0100

    Add cost share enums, and tests for validations, associations, and enums

--- app/models/cost_share.rb
+++ app/models/cost_share.rb
@@ -1,3 +1,26 @@
 class CostShare < ApplicationRecord
   belongs_to :scope, polymorphic: true
+  belongs_to :linked_cost_share, class_name: "CostShare", optional: true
+
+  enum :cost_share_type, {
+    deductible: 0,
+    excess: 1,
+    coinsurance: 2
+  }
+
+  enum :unit, {
+    amount: 0,
+    percent: 1
+  }
+
+  enum :per, {
+    per_visit: 0,
+    per_condition: 1,
+    per_year: 2
+  }
+
+  validates :scope, presence: true
+  validates :cost_share_type, presence: true
+  validates :unit, presence: true
+  validates :per, presence: true
 end
