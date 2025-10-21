81b6a1e3e2655fdfcc02ca0ef7c6037a16cf063a
Austin Mason <austinpmason@gmail.com>
2025-10-21 19:26:34 +0100

    feat(api): add insurers controller with full CRUD actions
    Introduce InsurersController with standard RESTful actions: index, show,
    new, edit, create, update, and destroy. Implements strong parameters and
    follows Rails best practices for controller structure.

---
+++ app/controllers/insurers_controller.rb
@@ -0,0 +1,50 @@
+class InsurersController < ApplicationController
+  before_action :set_insurer, only: %i[show edit update destroy]
+
+  def index
+    @insurers = Insurer.order(:name)
+  end
+
+  def show
+  end
+
+  def new
+    @insurer = Insurer.new
+  end
+
+  def edit
+  end
+
+  def create
+    @insurer = Insurer.new(insurer_params)
+
+    if @insurer.save
+      redirect_to @insurer, notice: "Insurer was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @insurer.update(insurer_params)
+      redirect_to @insurer, notice: "Insurer was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @insurer.destroy
+    redirect_to insurers_url, notice: "Insurer was successfully destroyed."
+  end
+
+  private
+
+    def set_insurer
+      @insurer = Insurer.find(params[:id])
+    end
+
+    def insurer_params
+      params.require(:insurer).permit(:name, :jurisdiction)
+    end
+end
