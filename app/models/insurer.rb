edbbed81f06b3acf4f18b912d12e106ec5a09c23
Austin Mason <austinpmason@gmail.com>
2025-10-22 16:48:50 +0100

    feat(models): add country_name method to insurer model
    Add country_name instance method to Insurer for locale-aware country
    name resolution using ISO3166. Includes tests for translation, common
    name, and ISO short name fallbacks.

--- app/models/insurer.rb
+++ app/models/insurer.rb
@@ -4,4 +4,9 @@ class Insurer < ApplicationRecord
 
   validates :name, presence: true
   validates :jurisdiction, presence: true
+
+  def country_name
+    country = ISO3166::Country[jurisdiction]
+    country.translations[I18n.locale.to_s] || country.common_name || country.iso_short_name
+  end
 end
