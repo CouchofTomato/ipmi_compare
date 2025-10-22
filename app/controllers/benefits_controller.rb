cba3ada881e1b46c569c4120725644a71cdeaed3
Austin Mason <austinpmason@gmail.com>
2025-10-22 21:01:41 +0100

    feat(controller): add benefits controller with CRUD actions
    Add BenefitsController with standard CRUD actions following Rails best practices.
    Implements strong parameters and before_action for resource loading.
    This enables management of Benefit resources via RESTful endpoints.

---
+++ app/controllers/benefits_controller.rb
@@ -0,0 +1,50 @@
+class BenefitsController < ApplicationController
+  before_action :set_benefit, only: %i[show edit update destroy]
+
+  def index
+    @benefits = Benefit.order(:name)
+  end
+
+  def show
+  end
+
+  def new
+    @benefit = Benefit.new
+  end
+
+  def edit
+  end
+
+  def create
+    @benefit = Benefit.new(benefit_params)
+
+    if @benefit.save
+      redirect_to @benefit, notice: "Benefit was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @benefit.update(benefit_params)
+      redirect_to @benefit, notice: "Benefit was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @benefit.destroy
+    redirect_to benefits_path, notice: "Benefit was successfully deleted."
+  end
+
+  private
+
+    def set_benefit
+      @benefit = Benefit.find(params[:id])
+    end
+
+    def benefit_params
+      params.require(:benefit).permit(:name, :category, :description)
+    end
+end
