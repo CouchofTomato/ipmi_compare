5c3c5b72f25044349a56f0a284182c92ba77fa00
Austin Mason <austinpmason@gmail.com>
2025-11-20 19:02:35 +0000

    feat(db): add default values and constraints to position columns
    - Set default value of 0 for position in coverage_categories
    - Set default value of 0 and make position non-nullable in module_groups
    - Update schema.rb to reflect the migration changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_20_185925) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_20_190122) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -81,7 +81,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_20_185925) do
   create_table "coverage_categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name", null: false
-    t.integer "position", null: false
+    t.integer "position", default: 0, null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_coverage_categories_on_name", unique: true
     t.index ["position"], name: "index_coverage_categories_on_position"
@@ -135,7 +135,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_20_185925) do
     t.text "description"
     t.string "name", null: false
     t.bigint "plan_id", null: false
-    t.integer "position"
+    t.integer "position", default: 0, null: false
     t.datetime "updated_at", null: false
     t.index ["plan_id"], name: "index_module_groups_on_plan_id"
   end
